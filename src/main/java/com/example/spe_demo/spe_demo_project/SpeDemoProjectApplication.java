23731574116ac23d81a8abb08561d2f4699abac3
preyasgarg <preyasgarg01@gmail.com>
2022-03-15 10:56:15 +0530

    first commit

---
+++ src/main/java/com/example/spe_demo/spe_demo_project/SpeDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spe_demo.spe_demo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeDemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spe_demo/spe_demo_project/SpeDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spe_demo.spe_demo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
